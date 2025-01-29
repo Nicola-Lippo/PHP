beb860cae95b16839c944ed93892d63e07e73a01
Nicola-Lippo <nicolalippo8991@gmail.com>
2025-01-29 16:40:56 +0100

    [UPD] snack intro

---
+++ snack/0_snack_intro/form.php
@@ -0,0 +1,24 @@
+<?php
+$p = $_GET['paragrafo'];
+$badwords = $_GET['badwords'];
+//paragrafo censurato con parola sostituita
+$censored_text = str_replace($badwords, '***', $p);
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>form ricevuto</title>
+</head>
+
+<body>
+    <div><?php echo 'Il paragrafo è: ' . $p; ?></div>
+    <div><?php echo 'Ed è lungo: '  . strlen($p) . ' caratteri'; ?></div>
+    <hr>
+    <div><?php echo 'Il paragrafo censurato è: ' . $censored_text; ?></div>
+    <div><?php echo 'Ed è lungo: '  . strlen($censored_text) . ' caratteri'; ?></div>
+</body>
+
+</html>
---
+++ snack/0_snack_intro/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Parole da censurare</title>
+</head>
+
+<body>
+    <form action="form.php" method="GET">
+        <h3>Compila il form</h3>
+        <div>
+            <label for="p">Paragrafo</label>
+            <input type="text" name="paragrafo" id="p">
+        </div>
+        <hr>
+        <div>
+            <label for="bad">Badwords</label>
+            <input type="text" name="badwords" id="bad">
+        </div>
+        <hr>
+        <div>
+            <button>Sent</button>
+        </div>
+    </form>
+</body>
+
+</html>
