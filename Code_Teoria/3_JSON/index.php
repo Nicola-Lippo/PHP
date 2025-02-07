d631746db81e4e7047a087565789f1efe485034f
Nicola-Lippo <nicolalippo8991@gmail.com>
2025-02-07 19:16:33 +0100

    [UPD] json_decode

--- Code_Teoria/3_JSON/index.php
+++ Code_Teoria/3_JSON/index.php
@@ -1,55 +1,8 @@
 <?php
-$dati_scuola = [
-    "studenti" => [
-        [
-            "nome" => "Marco",
-            "cognome" => "Rossi",
-            "eta" => 20
-        ],
-        [
-            "nome" => "Giulia",
-            "cognome" => "Bianchi",
-            "eta" => 22
-        ],
-        [
-            "nome" => "Luca",
-            "cognome" => "Verdi",
-            "eta" => 19
-        ],
-        [
-            "nome" => "Anna",
-            "cognome" => "Ferrari",
-            "eta" => 21
-        ],
-        [
-            "nome" => "Davide",
-            "cognome" => "Esposito",
-            "eta" => 23
-        ]
-    ],
-    "insegnanti" => [
-        [
-            "nome" => "Mario",
-            "cognome" => "Rossi",
-            "materia" => "Matematica"
-        ],
-        [
-            "nome" => "Laura",
-            "cognome" => "Bianchi",
-            "materia" => "Inglese"
-        ],
-        [
-            "nome" => "Giovanni",
-            "cognome" => "Verdi",
-            "materia" => "Storia"
-        ]
-    ]
-];
-//header()
-#specificare che le informazioni restituite sono di tipo json a JS
-#aggiunge le intestazioni indicate nella risposta HTTP.
-header('Content-Type: application/json');
-//json_encode
-#La funzione json_encode() traduce qualsiasi tipo di dato in formato json.
-#trasformare gli array php in oggetti json!
-echo json_encode($dati_scuola);
+//Per poter leggere il contenuto di un file usiamo file_get_contents() che ci restituisce una stringa.
+$dati_scuola = file_get_contents('dati_scuola.json');
+echo $dati_scuola;
+//json_decode
+#traduce una stringa da formato json in una variabile PHP
+$array_scuola = json_decode($dati_scuola, true);
+var_dump($array_scuola);
