20b6f325427ad3551f603b4fa0040139d2f205da
Nicola-Lippo <nicolalippo8991@gmail.com>
2025-02-08 14:06:45 +0100

    [UPD} Json + logica

--- Code_Teoria/3_JSON/index.php
+++ Code_Teoria/3_JSON/index.php
@@ -1,8 +1,17 @@
 <?php
 //Per poter leggere il contenuto di un file usiamo file_get_contents() che ci restituisce una stringa.
 $dati_scuola = file_get_contents('dati_scuola.json');
-echo $dati_scuola;
 //json_decode
 #traduce una stringa da formato json in una variabile PHP
+#true: il secondo parametro dice alla funzione di trasformare gli oggetti JSON in array associativi php.
+#elaborazione del dato
 $array_scuola = json_decode($dati_scuola, true);
-var_dump($array_scuola);
+#crud
+//header()
+#specificare che le informazioni restituite sono di tipo json a JS
+#aggiunge le intestazioni indicate nella risposta HTTP.
+header('Content-Type: application/json');
+//json_encode
+#La funzione json_encode() traduce qualsiasi tipo di dato in formato json.
+#trasformare gli array php in oggetti json!
+echo json_encode($array_scuola);
